f0110945ab9bfc851e59e188c718e77ea13a871a
Valery <arkhangel4949@gmail.com>
2018-09-12 12:25:22 +0300

    alpha 1

--- src/index.js
+++ src/index.js
@@ -4,4 +4,26 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var num=0,i,firstSpichonee=0,secondSpichonee=0,thirdSpichonee=0;
+  for(i=0; i<preferences.length; i++){
+
+  	if(preferences[i]!==undefined){
+  			firstSpichonee=preferences[i];
+  	}else{continue;}
+
+  	thirdSpichonee=i+1;
+
+  	if(firstSpichonee<preferences.length && preferences[firstSpichonee-1]!==undefined){
+
+  			secondSpichonee=preferences[firstSpichonee-1];
+
+  			if(preferences[secondSpichonee-1]===thirdSpichonee){  				
+  					num++;
+  					delete preferences[i];
+  					delete preferences[firstSpichonee-1];				
+  					delete preferences[secondSpichonee-1];		
+  			}else{continue;}
+  	}else{continue;}
+  }
+  return num;
 };
